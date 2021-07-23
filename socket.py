94f2f27f5d7312d67ff716a0ec3cd828bf28583c
Aftab Ali <54621320+aftab-ali@users.noreply.github.com>
2021-07-23 06:37:12 -0700

    Update socket.py

--- socket.py
+++ socket.py
@@ -5,6 +5,9 @@ import socket
 HOST = '127.0.0.1'
 PORT = 2525
 
+# AF_INET is IPV4
+# SOCK_STREAM is for TCP
+# SOCK_DGRAM is for UDP
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 soc.connect((HOST, PORT))
 
